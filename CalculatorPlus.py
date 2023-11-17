663db3ba581445e7c1cac411407a685aeeb3367b
ankuronlyme <ankur.onlymee@gmail.com>
2023-11-17 21:46:23 +0530

    commit my code

--- CalculatorPlus.py
+++ CalculatorPlus.py
@@ -17,12 +17,18 @@ class Calculator:
     def divide(self, a, b):
 
         return a / b
-    
+
+    def square_root(self, x):
+
+        return math.sqrt(x)
+
 calculator = Calculator()
 
 num1 = 16
 
 num2 = 4
+
+num3 = 25
     
 print(f"{num1} + {num2} = {calculator.add(num1, num2)}")
 
@@ -31,3 +37,7 @@ print(f"{num1} - {num2} = {calculator.subtract(num1, num2)}")
 print(f"{num1} * {num2} = {calculator.multiply(num1, num2)}")
 
 print(f"{num1} / {num2} = {calculator.divide(num1, num2)}")
+
+print(f"The square root of {num3} = {calculator.square_root(num3)}")
+
+
